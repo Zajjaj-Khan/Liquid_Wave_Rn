f2e5619abf4acab6bdb1af3167375f2d5b11242e
Zajjaj-Khan <kzajjaj@gmail.com>
2024-09-27 01:36:11 +0500

    Building the Intro Page

---
+++ components/Slide3.tsx
@@ -0,0 +1,40 @@
+// Slide3.js
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const Slide3 = () => {
+  return (
+    <View style={[styles.container, { backgroundColor: "#F2A1AD" }]}>
+      <Image source={require('../assets/1.png')} style={styles.image} />
+      <Text style={styles.title}>Dessert Recipes</Text>
+      <Text style={styles.description}>
+        Hot or cold, our dessert recipes can turn an average meal into a memorable event.
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+});
+
+export default Slide3;
